c526894361b71478de63a109768ce7b8e6b1fc7b
Farshid Torkaman <farshidtorkaman@outlook.com>
2020-10-23 13:22:56 +0330

    first commit

---
+++ NLHospital/NLHBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace NLHospital
+{
+    public class NLHBase : Form
+    {
+        protected override void Dispose(bool disposing)
+        {
+            base.Close();
+        }
+    }
+}
---
+++ NLHospital/frmAdmissions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NLHospital
+{
+    internal class FrmAdmissions
+    {
+        public bool Visible { get; internal set; }
+
+        internal void Activate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
